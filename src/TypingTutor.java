db322bb56e6e55a27bab62c8737fe53a1598c96f
nealparth <league@iMac-5.attlocal.net>
2019-05-02 19:55:59 -0700

    TypingTutor,BookOfIllusions

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,13 +1,41 @@
+import java.awt.event.KeyListener;
+import java.util.Random;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class TypingTutor {
-public static void main(String[] args) {
+	char currentLetter = generateRandomLetter();
+
 	
+public static void main(String[] args) {
 }
 
 void type() {
 	JFrame frame= new JFrame();
 	frame.setVisible(true);
 	frame.pack();
+	JLabel label= new JLabel();
+label.add(frame);
+	label.equals(currentLetter);
+	label.setFont(label.getFont().deriveFont(28.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
+	frame.addKeyListener(null);
+	label.resetKeyboardActions();
+	label.updateUI();
+	
+	
+	
 }
+void KeyReleased() {
+
+	
 }
+
+char generateRandomLetter() {
+    Random r = new Random();
+    return (char) (r.nextInt(26) + 'a');
+}
+
+}
+
--- src/TypingTutorRunner.java
+++ src/TypingTutorRunner.java
@@ -1,9 +1,11 @@
 
 public class TypingTutorRunner {
-	
+	char currentLetter;
 	public static void main(String[] args) {
 		TypingTutor type= new TypingTutor();
 		type.type();
+		
+
 	}
 
 }
