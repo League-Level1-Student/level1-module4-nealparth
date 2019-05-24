e08506f15bc19fff8f8879d47b24080c24c83fba
League Student <league@wtsim17.attlocal.net>
2019-05-23 19:46:14 -0700

    Calculator(IP)

--- src/fortune_teller/Calculator.java
+++ src/fortune_teller/Calculator.java
@@ -13,10 +13,14 @@ public class Calculator implements ActionListener {
 
 JButton button1 = new JButton("add");
 JButton button2= new JButton("sub");
+JButton button3= new JButton("mul");
+JButton button4= new JButton("div");
 int textfield;
 int textfield1;
 JTextField field = new JTextField(12);
 JTextField field1= new JTextField(12);
+JLabel label= new JLabel();
+
 void calculator1() {
 	JPanel panel= new JPanel();
 	JFrame frame= new JFrame();
@@ -26,12 +30,21 @@ void calculator1() {
 	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 	panel.add(button1);
 	panel.add(button2);
+	panel.add(button3);
+	panel.add(button4);
 	button1.addActionListener(this);
 	button2.addActionListener(this);
+	button3.addActionListener(this);
+	button4.addActionListener(this);
 	button1.setSize(1, 1);
 	button2.setSize(1,1);
+	button3.setSize(1,1);
+	button4.setSize(1,1);
 	panel.add(field);
 	panel.add(field1);
+	panel.add(label);
+	
+
 
 	 frame.setVisible(true);
 	 frame.pack();
@@ -49,6 +62,15 @@ void calculator1() {
 		return five;
 		
 	}
+	public int multiply(int one6, int one7) {
+		int eight= one6 * one7;
+		return eight;
+		
+	}
+	public int divide(int one9, int one10) {
+		int eleven= one9/one10;
+		return eleven;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -57,12 +79,26 @@ void calculator1() {
 		String text1= field1.getText();
 		 textfield= Integer.parseInt(text);
 		 textfield1= Integer.parseInt(text1);
-		if(button1.equals(e)) {
-			int added=	add(textfield,textfield1);
+		 int added;
+		 int subtract;
+		 int multiply;
+		 int divide;
+	
+		 if(button1.equals(e)) {
+		 added=	add(textfield,textfield1);
+			
 		}
 		else if (button2.equals(e)) {
-			int subtract= subtract(textfield,textfield1);
+			 subtract= subtract(textfield,textfield1);
 		}
+		else if(button3.equals(e)) {
+			 multiply= multiply(textfield, textfield1);
+		}
+		else if(button4.equals(e)) {
+			 divide= divide(textfield, textfield1);
+		}
+		
+		
 		
 	} 
 	
